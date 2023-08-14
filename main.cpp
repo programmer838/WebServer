e27865b6db1432ae8627c23193ba439eb9f76dd3
programmer838 <71629437+programmer838@users.noreply.github.com>
2023-08-14 19:05:12 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,49 +1,50 @@
+#include <condition_variable>
+#include <functional>
 #include <iostream>
+#include <vector>
+#include <memory>
+#include <thread>
+#include <array>
+#include <queue>
+#include <mutex>
+
+#include <sys/socket.h>
 #include <liburing.h>
+
+class thread_pool {
+
+public:
+    thread_pool() : pool(std::make_unique<std::thread[]>(std::thread::hardware_concurrency())) {
+
+    }
+
+    thread_pool(const thread_pool& p) = delete;           // Disable copy constructor
+    thread_pool(thread_pool&& p) = delete;                // Disable move constructor
+    thread_pool& operator=(const thread_pool&) = delete;  // Disable copy assignment operator
+    thread_pool& operator=(const thread_pool&&) = delete; // Disable move assignment operator
+
+
+    void push_task(std::function<void()> function) {
+
+         {                                                            
+            const std::scoped_lock<std::mutex> scoped_lock(mutex);    // Aquire mutex in block scope
+            task_queue.push(function);                             // Add task to task queue
+         }
+                                                                  // At this point RAII lock has released mutex since we are out of block scope
+         conditional_variable.notify_one();                       // Notify/wakeup thread that task is available
+    }
+
+private:
+    std::queue<std::function<void()>> task_queue; 
+    std::unique_ptr<std::thread[]> pool;          
+    std::condition_variable conditional_variable;
+    std::mutex mutex;   
+};
 
 int main(int argc, char **argv)
 {
+    struct io_uring ring;
+    io_uring_queue_init(10, &ring, 0);
 
-    
     return 0;
 }
-
-/*
-
-Aims:
-
-    Emphasize Network Programming: Implement basic client-server communication using TCP/IP in C++.
-    Focus on Multithreading: Explore multithreading techniques and design a server that handles multiple client connections simultaneously.
-
-Background:
-
-    Networking Basics: Work with TCP/IP protocols to handle client-server communication.
-    Multithreading Concepts: Understand and implement threading models to manage multiple client connections efficiently.
-
-Early Deliverables:
-
-    Basic Network Server: A single-threaded server that can handle one client at a time using a simple custom protocol over TCP/IP.
-    Network Programming Report: Detailing the network architecture, protocols used, and C++ implementation specifics.
-
-Advanced Features:
-
-    Multithreaded Architecture: Upgrade the server to a multithreaded model using C++ threading libraries, allowing simultaneous handling of multiple clients.
-    Simple Request-Response Protocol: Implement a minimal protocol to receive and respond to basic client requests (e.g., sending a welcome message or echoing client input).
-    Performance Optimization: Apply C++ techniques to reduce latency and improve server performance, focusing on efficient thread management.
-
-Final Deliverables:
-
-    Fully Operational Multithreaded Network Server: Capable of efficiently handling multiple simultaneous client connections, adhering to a minimal protocol for simple interaction.
-    Comprehensive Documentation: Including the network programming techniques, C++ threading implementation, and clear instructions for setup, use, and troubleshooting.
-    Performance Analysis Reports: Assessing the server's efficiency, latency, and scalability.
-
-Relevance to Network Programming and HFT:
-
-    Networking Mastery: In-depth work with TCP/IP and custom protocol provides hands-on experience with core network programming concepts in C++.
-    Multithreading Skills: The development of a multithreaded server architecture aligns with the concurrent processing requirements of HFT systems.
-
-Technology Stack:
-
-    Programming Language: C++, capitalizing on modern C++ features and libraries for network and threading tasks.
-    Networking Libraries: Utilize suitable C++ networking libraries like Boost.Asio or standard networking facilities in C++17 or later.
-*/
