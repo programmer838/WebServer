b89f115228fd6458fe5942bd21490f6384631eef
programmer838 <71629437+programmer838@users.noreply.github.com>
2023-08-28 12:52:17 +0100

    Replace std::bind() with lambdas

--- ring_buffer.hpp
+++ ring_buffer.hpp
@@ -5,62 +5,28 @@
 #include <utility>
 #include <stdexcept>
 
-template <typename T>
+template <typename T, uint32_t ring_buffer_size>
 class ring_buffer {
 
 public:
-    T *queue;  
-    int ssize; 
+    T queue[ring_buffer_size];    
     int count;
     int head;
     int tail;
     
-    ring_buffer(int k) : ssize(k), count(0), head(0), tail(-1) {
-        queue = new T[k];
-    }
+    ring_buffer() : count(0), head(0), tail(-1) {}
  
-    ring_buffer(ring_buffer&& p) noexcept {                        // Move constructor
-	
-	this->queue = std::move(p.queue);
-	this->ssize  = p.ssize;
-	this->count = p.count;
-	this->head  = p.head;
-        this->tail  = p.tail;
-
-	p.queue = nullptr;
-	p.ssize  = 0;
-        p.count = 0;
-        p.head  = 0;
-        p.tail  = 0;	     
-    }
-
-    ring_buffer& operator=(ring_buffer&& p) noexcept {         // Move assignment operator
-    	if (this == &p) return *this;
-
-	this->queue = std::move(p.queue);
-        this->ssize  = p.ssize;
-        this->count = p.count;
-        this->head  = p.head;
-        this->tail  = p.tail;
-	
-	p.queue = nullptr;
-        p.ssize  = 0;
-        p.count = 0;
-        p.head  = 0;
-        p.tail  = 0;
-
-	return *this;
-    }
-
+    ring_buffer(ring_buffer&& p) = delete;                // Delete Move constructor
+    ring_buffer& operator=(ring_buffer&& p) = delete;     // Delete move assignment operator
     ring_buffer(const ring_buffer& p) = delete;           // Disable copy constructor
     ring_buffer& operator=(const ring_buffer&) = delete;  // Disable copy assignment operator
 
     bool push(T value) noexcept {
          
-         if (count == ssize)
+         if (count == ring_buffer_size)
              return false;
         
-         tail = (tail + 1) % ssize;
+         tail = (tail + 1) % ring_buffer_size;
          queue[tail] = value;
          count++;
 
@@ -72,27 +38,23 @@ public:
          if (count == 0)
              return false;
 
-         head = (head + 1) % ssize;
+         head = (head + 1) % ring_buffer_size;
          count--;
          
          return true;
     }
 
-    T& front() const {
+    T& front() {
         if (count == 0)
 	    throw std::runtime_error("ring_buffer Runtime Error: failed to retrieve Front() because buffer is empty.\n"); 
 	return queue[head];
     }
 
     bool empty() const noexcept {
-         return count == 0;
+        return count == 0;
     }
 
     int size() const noexcept {
 	return count;
-    }
-   
-    ~ring_buffer() {
-	delete[] queue;
     }
 };
--- thread_pool.hpp
+++ thread_pool.hpp
@@ -1,21 +1,27 @@
 #pragma once
 
-#include <condition_variable>
-#include <functional>
-#include <iostream>
-#include <vector>
-#include <memory>
-#include <atomic>
-#include <thread>
-#include <queue>
 #include <mutex>
+#include <thread>
+#include <atomic>
+#include <memory>
+#include <vector>
+#include <iostream>
+#include <functional>
+#include <condition_variable>
 
 #include "ring_buffer.hpp"
 
+template <uint32_t threads, uint32_t task_pool_size>
 class thread_pool {
 
-public:
-    thread_pool(uint32_t threads = std::thread::hardware_concurrency(), uint32_t task_pool_size = std::thread::hardware_concurrency());
+public: 
+    thread_pool() : number_of_threads(threads), tasks_processed(0), stop(false)
+    {
+	pool = std::make_unique<std::thread[]>(number_of_threads);
+
+        for (unsigned int i = 0; i < number_of_threads; i++)
+             pool.get()[i] = std::thread(&thread_pool::do_work, this); 
+    }
 
     thread_pool(const thread_pool& p) = delete;           // Disable copy constructor
     thread_pool(thread_pool&& p) = delete;                // Disable move constructor
@@ -28,32 +34,79 @@ public:
 	    bool ret;		
 
             {
-                 mutex.lock();
-                 ret = task_queue.push(std::bind(std::forward<Callable>(function), std::forward<Args>(args)...));  // Add task to task queue
+                 mutex.lock();	
+		 auto f = [function = std::forward<Callable>(function), ...args = std::forward<Args>(args)](){ function(args...); };
+		 // Use lambda instead of std::bind
+		 ret = task_queue.push(f);
             	 mutex.unlock();
 	    }
-                                                                    // At this point RAII lock has released mutex since we are out of block scope
+
             conditional_variable.notify_one();                       // Notify/wakeup thread that task is available
 	    return ret;	    
     }
 
-    void destroy_thread_pool();
 
-    unsigned long get_task_queue_size();
+    void destroy_thread_pool() {
 
-    uint64_t get_number_tasks_processed();
+        mutex.lock();
+        stop = true;
+        mutex.unlock();
 
-    ~thread_pool();
+        conditional_variable.notify_all();     // Wake up every thread that is sleeping
+
+        for (int i = 0; i < number_of_threads; i++) { 
+                if (pool.get()[i].joinable())  
+                    pool.get()[i].join();         // To kill each thread we need to join it, this makes the calling thread (main thread) wait for it to 
+                                                  // Complete its task and eventually terminate, this means we must wait for every thread doing work to 
+                                                  // Finish and doing .join() allows us to do this
+        }
+    }
+
+    unsigned long get_task_queue_size() {  // Return size of current task queue for logging purposes
+
+        mutex.lock();
+        unsigned long task_queue_size = task_queue.size();
+        mutex.unlock();
+        return task_queue_size;
+    }
+
+    uint64_t get_number_tasks_processed() {      // Return total tasks processed for logging purposes
+
+        mutex.lock();
+        return tasks_processed;
+        mutex.unlock();    
+    }
+
+    ~thread_pool() {
+        destroy_thread_pool();
+    }
+
+    void do_work() {
+
+        std::function<void()> task;
+
+        while (true) {
+
+            {
+                std::unique_lock unique_lock(mutex);
+                conditional_variable.wait(unique_lock, [&](){return stop || !task_queue.empty();}); // Handle spurious wakeups and wait if condition is false
+                if (stop && task_queue.empty()) break;                                              // Upon termination only terminate after the remaining 
+                                                                                                    // Tasks have been handled
+                task = std::move(task_queue.front());
+                task_queue.pop();
+            }
+
+            task();
+            ++tasks_processed;
+        }
+    }
 
 private:
-    ring_buffer<std::function<void()>> task_queue; 
-    std::unique_ptr<std::thread[]> pool;          
+    ring_buffer<std::function<void()>, task_pool_size> task_queue;
+    std::unique_ptr<std::thread[]> pool;
     std::condition_variable conditional_variable;
-    std::mutex mutex;   
+    std::mutex mutex;
     uint32_t number_of_threads;
     std::atomic<uint64_t> tasks_processed;
     bool stop;
-
-    void do_work();
 };
-
